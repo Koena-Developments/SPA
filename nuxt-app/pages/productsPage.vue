b228d3fd05cd4bd2ab7aedebad2b898aafd8dd33
Thabang Motswenyane <thabangmotswenyane511@gmail.com>
2025-05-06 13:02:49 +0200

    added an image

--- nuxt-app/pages/productsPage.vue
+++ nuxt-app/pages/productsPage.vue
@@ -1,26 +1,49 @@
 <template>
   <div>
     <h2>Products Page</h2>
+
+    <!-- make a card -->
     <div v-for="product in products" :key="product.id">
-      <p>here are the products {{ product.title }}</p>
+      <p>Here are the products: {{ product.title }} {{ product.id }}  </p>
+      <img :src="product.image" alt="">
+    
+    
+    
     </div>
-  </div>
+
+
+
+
+
+
+</div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
 
-const products = ref([])
 
-async function getProducts(url) {
-  const response = await fetch(url)
-  products.value = await response.json()
-}
+/*
+UPDATES
+useFetch() -> automatic handling of SSR
+           -> automatic handling reactivity
+           -> automatic json parsing
 
-onMounted(() => {
-  getProducts('https://fakestoreapi.com/products')
-})
+
+use (useFetch instead of fetch)
+
+currently using fetch() -> manual handling SSR
+                        -> manually parse response to .json 
+                        -> !reactive
+
+Steps
+1 -> change from fetch(url) to useFetch url for auto handling of json parsing and SSR
+*/
+
+// get
+const {data: products} = await useFetch('https://fakestoreapi.com/products')
+
+
 </script>
-
 <style>
 </style>
+
