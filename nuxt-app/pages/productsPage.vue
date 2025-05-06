b3f845568c71179c52e772b7a75efef2e0f715db
Your Name <you@example.com>
2025-05-06 05:10:42 +0200

    rendering data frrom fakeStoreAPI

---
+++ nuxt-app/pages/productsPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>Products Page</h2>
+    <div v-for="product in products" :key="product.id">
+      <p>here are the products {{ product.title }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const products = ref([])
+
+async function getProducts(url) {
+  const response = await fetch(url)
+  products.value = await response.json()
+}
+
+onMounted(() => {
+  getProducts('https://fakestoreapi.com/products')
+})
+</script>
+
+<style>
+</style>
